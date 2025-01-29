be5ceffc2c1a3a1b337f9bb22c096d0b5aa523f5
jonathanbsousa <jonathanbsousa55@gmail.com>
2025-01-29 16:47:37 -0300

    29/01/2025 V2

--- PWBE_2/Projeto_01/back/api/urls.py
+++ PWBE_2/Projeto_01/back/api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import listar_professores, ProfessoresView, ProfessoresDatailsView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('professores', listar_professores),
     path('prof', ProfessoresView.as_view()),
-    path('professor/<int:pk>', ProfessoresDatailsView.as_view())
+    path('professor/<int:pk>', ProfessoresDatailsView.as_view()),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
