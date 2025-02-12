22a3cd2601cc2e6e1ac87eb0b0bf8efb53f951f6
jonathanbsousa <jonathanbsousa55@gmail.com>
2025-02-12 17:00:35 -0300

    12/02/2025

---
+++ PWBE_2/Projeto_01/front/vite-project/src/pages/home/index.jsx
@@ -0,0 +1,145 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { FaEdit, FaTrash, FaPlus, FaSearch } from 'react-icons/fa'
+import './styles.css'
+import Header from "../../components/header";
+import Footer from "../../components/footer";
+
+
+export default function Home() {
+    const [dados, setDados] = useState([])
+    const token = localStorage.getItem('token')
+    console.log("Token Home", token)
+
+    useEffect(() => {
+
+        if (!token) return;
+
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://127.0.0.1:8000/api/professores',
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        }
+                    }
+                )
+                setDados(response.data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        fetchData()
+    }, [dados])
+
+    const apagar = async (id) => {
+        if (window.confirm("Tem certeza? ")) {
+            try {
+                await axios.delete(`http://127.0.0.1:8000/api/professor/${id}`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        }
+                    }
+                )
+                setDados(dados.filter((professor) => { professor.id !== id }))
+            } catch (error) {
+                console.error(error)
+            }
+        }
+    }
+
+    return (
+        <div >
+            <Header />
+            <div className="container_home">
+                <div className="flex_table">
+
+                
+                <div className="lista">
+                    <table>
+                        <thead>
+                            <tr className="campos">
+                                <div className="col1">
+                                    <th></th>
+                                </div>
+                                <div className="col2">
+                                    <th></th>
+                                </div>
+                                <div className="col3">
+                                    <th>ID</th>
+                                    </div>
+                                <div className="col4">
+                                    <th>NI</th>
+                                    </div>
+                                <div className="col5">
+                                    <th>Nome</th>
+                                    </div>
+                                <div className="col6">
+                                    <th>Email</th>
+                                    </div>
+                                <div className="col7">
+                                    <th>Tel</th>
+                                    </div>
+                                <div className="col8">
+                                    <th>OC</th>
+                                    </div>
+                                <div className="col8">
+                                </div>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {dados.map((professor) => (
+                                <tr key={professor.id} className="campos">
+                                    <td className="icons">
+                                        <div className="col1">
+                                            <FaEdit className="edit" />
+                                        </div>
+                                        <div className="col2">
+                                            <FaTrash className="delete" onClick={() => apagar(professor.id)} />
+                                        </div>
+                                    </td>
+
+                                    <div className="col3">
+                                        <td>{professor.id}</td>
+                                    </div>
+                                    <div className="col4">
+                                        <td>{professor.ni}</td>
+                                    </div>
+                                    <div className="col5">
+                                        <td>{professor.nome}</td>
+                                    </div>
+                                    <div className="col6">
+                                        <td>{professor.email}</td>
+                                    </div>
+                                    <div className="col7">
+                                        <td>{professor.tel}</td>
+                                    </div>
+                                    <div className="col8">
+                                        <td>{professor.ocupacao}</td>
+                                    </div>
+
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+
+                <div className="footer_table">
+                    <div className="btn1">
+                        <FaPlus className="adicionar" />
+                    </div>
+                    <div className="pesquisar">
+                        <input placeholder="id" />
+                    </div>
+                    <div className="btn2">
+                        <FaSearch className="procurar" />
+                    </div>
+                </div>
+            </div>
+            </div>
+            <Footer />
+        </div>
+    )
+}
